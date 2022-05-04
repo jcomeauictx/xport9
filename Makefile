0c32bf458c84b6f054e5b678b28159b80fef7a45
John Comeau <jc@unternet.net>
2022-05-04 13:40:22 -0600

    getting started

--- Makefile
+++ Makefile
@@ -1,8 +1,13 @@
 SOURCES := $(wildcard *.py)
 LINT := $(SOURCES:.py=.lint)
 TESTS := $(SOURCES:.py=.test)
+PYTHON ?= python3
 PYLINT ?= pylint3
 export
 all: test.csv
 %.csv: xport.py %.xpt $(LINT) $(TESTS)
-	./$< $(words 1, $+)
+	$(PYTHON) $< $(word 2, $+)
+%.lint: %.py
+	$(PYLINT) $<
+%.test: %.py
+	$(PYTHON) -m doctest $<
--- xport.py
+++ xport.py
@@ -5,6 +5,9 @@ SAS .xpt (transport) versions 8 and 9 converter
 import sys
 
 def xpt_to_csv(filename=None, outfilename=None):
+    '''
+    convert xpt file to csv format
+    '''
     infile = open(filename, 'rb') if filename is not None else sys.stdin
     outfile = open(outfilename, 'w') if outfilename is not None else sys.stdout
     infile.close()
