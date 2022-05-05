e31c3b4b2b2e883da5c6e6072621932c70d0d0aa
John Comeau <jc@unternet.net>
2022-05-04 21:38:26 -0600

    working on float conversion

--- xport.py
+++ xport.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python3
 '''
 SAS .xpt (transport) versions 8 and 9 converter
+
+https://support.sas.com/content/dam/SAS/support/en/technical-papers/
+record-layout-of-a-sas-version-8-or-9-data-set-in-sas-transport-format.pdf
 '''
-import sys, csv, logging  # pylint: disable=multiple-imports
+import sys, csv, struct, logging  # pylint: disable=multiple-imports
+logging.basicConfig(level=logging.DEBUG if __debug__ else logging.INFO)
+
+TESTVECTORS = {
+    # from PDF referenced above
+    'xpt': {
+        1: b'\x41\x10\0\0\0\0\0\0',
+        -1: b'\xc1\x10\0\0\0\0\0\0',
+        0: b'\0\0\0\0\0\0\0\0',
+        2: b'\x41\x20\0\0\0\0\0\0'},
+    'ieee': {
+        1: b'\0\0\0\0\0\0\xf0\x3f',
+        -1: b'\0\0\0\0\0\0\xf0\xbf',
+        0: b'\0\0\0\0\0\0\0\0',
+        2: b'\0\0\0\0\0\0\0\x40'}
+}
 
 def xpt_to_csv(filename=None, outfilename=None):
     '''
@@ -18,6 +36,25 @@ def xpt_to_csv(filename=None, outfilename=None):
             break
         logging.debug('record: %r', record)
         csvout.writerow([record.rstrip(b'\0')])
+
+def ibm_to_double(bytestring, pack_output=False):
+    '''
+    convert 64-bit IBM float bytestring to IEEE floating point
+
+    IBM:  seeeeeeemmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+    IEEE: seeeeeeeeeeemmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+
+    >>> ibm = TESTVECTORS['xpt']
+    >>> ieee = TESTVECTORS['ieee']
+    >>> [ibm_to_double(ibm[key]) for key in sorted(ibm)]
+    [-1.0, 0, 1.0, 2.0]
+    >>> {key: ibm_to_double(ibm[key], True) for key in ibm} == ieee
+    True
+    '''
+    integer = struct.unpack('>Q', bytestring)[0]
+    sign, remainder = integer & 0x80000000, integer & 0x7fffffff
+    repacked = struct.pack('<Q', sign & (remainder >> 4))
+    return repacked if pack_output else struct.unpack('<d', repacked)[0]
 
 if __name__ == '__main__':
     xpt_to_csv(*sys.argv[1:])
